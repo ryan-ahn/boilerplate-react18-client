cc0c8c14ae75346809b1a4c5ca5373004c490483
ryan-ahn <kaaiinn4@gmail.com>
2023-08-02 23:55:35 +0900

    Modify: 패키지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Main from '@containers/main';
+
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Main />
     </>
   );
 }
---
+++ src/containers/main/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Author : Ryan
+ * Date : 2023-08-02
+ * Desc : index
+ */
+
+import styled from 'styled-components';
+
+export default function index() {
+  return <Wrapper>33</Wrapper>;
+}
+
+const Wrapper = styled.div``;

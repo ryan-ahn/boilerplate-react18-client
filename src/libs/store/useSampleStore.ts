a9a130103a6f3f0733ef066fced38daf866c7c87
ryan-ahn <kaaiinn4@gmail.com>
2023-08-03 05:28:21 +0900

    Add: 스토어 추가

---
+++ src/libs/store/useSampleStore.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { create } from 'zustand';
+
+export const useUserStore = create(set => ({
+  // State
+  data: null,
+  // Set State
+  setData: (payload: any) => set(() => ({ data: payload })),
+  // Fetch
+  fetchSample: async () => {
+    try {
+      await axios.get('url');
+    } catch (e: any) {
+      throw Error(e.message);
+    }
+  },
+}));
